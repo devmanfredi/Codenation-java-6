65d685702b40a22799176b09c9f6ab1776c90859
Heuler Felipe Ramos Manfredi <felipemanfredigalaxy@gmail.com>
2019-08-02 15:50:58 -0300

    Cifra de Júlio Cézar finalizada

--- src/main/java/challenge/CriptografiaCesariana.java
+++ src/main/java/challenge/CriptografiaCesariana.java
@@ -3,38 +3,41 @@ package challenge;
 import java.util.Iterator;
 
 public class CriptografiaCesariana implements Criptografia {
-    String normalString = new String("a ligeira raposa marrom saltou sobre o cachorro cansado");
-
-    String cifrado = criptografar(normalString);
-    String decifrado = descriptografar(cifrado);
-
     @Override
     public String criptografar(String texto) {
-        int i, n = texto.length();
-        int chave = 3;
-        String saux = "";
-
-        for (i = 0; i < n; i++) {
-            saux = saux + (char) (texto.charAt(i) + chave);
+        if (texto.length() == 0){
+            IllegalArgumentException error = new IllegalArgumentException();
+            throw error;
         }
-        return saux.toLowerCase();
-        //throw new UnsupportedOperationException("esse method nao esta implementado aainda");
+        String cifrado = "";
+        char cifra = 3;
+        for (char i : texto.toCharArray()){
+            if (Character.isLetter(i)){
+                char j = (char) (i + cifra);
+                cifrado = cifrado + (j);
+            }else {
+                cifrado = cifrado + i;
+            }
+        }
+        return cifrado.toLowerCase();
     }
 
     @Override
     public String descriptografar(String texto) {
-        char[] cifradoT = texto.toCharArray();
-        String decifrado = null;
-        int count = 3;
-
-        for (int i = 0; i < cifradoT.length; i++) {
-            Character character = cifradoT[i];
-            if (character != ' ' && character != ',' && character != '.') {
-                cifradoT[i] = (char) ((cifradoT[i] - 'a' + 26 - count) % 26 + 'a');
-                decifrado = new String(cifradoT);
+        if (texto.length() == 0){
+            IllegalArgumentException error = new IllegalArgumentException();
+            throw error;
+        }
+        String decifrado = "";
+        char cifra = 3;
+        for (char i : texto.toCharArray()){
+            if (Character.isLetter(i)){
+                char j = (char) (i - cifra);
+                decifrado = decifrado + (j);
+            }else {
+                decifrado = decifrado + i;
             }
         }
         return decifrado.toLowerCase();
-        //throw new UnsupportedOperationException("esse method nao esta implementado aainda");
     }
 }
